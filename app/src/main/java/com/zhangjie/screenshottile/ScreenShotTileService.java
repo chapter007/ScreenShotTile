a8aa66b25bca5def7dd18e674236a8b956ebb97f
zhangjie <zx.zhangjie@qq.com>
2017-02-04 21:55:20 +0800

    android tile

---
+++ app/src/main/java/com/zhangjie/screenshottile/ScreenShotTileService.java
@@ -0,0 +1,12 @@
+package com.zhangjie.screenshottile;
+
+import android.annotation.TargetApi;
+import android.service.quicksettings.TileService;
+
+/**
+ * Created by zhangjie on 2017/2/4.
+ */
+@TargetApi(24)
+public class ScreenShotTileService extends TileService {
+    
+}
